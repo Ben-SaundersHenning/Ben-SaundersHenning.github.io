72b7cd32c834ba7a00f2caa3e0d0db842cb3c2bc
Ben Saunders-Henning <ben.shp@proton.me>
2025-05-11 22:21:05 -0400

    Use grid for blog structure

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
 
   $("#test").text('hello this is a jquery test');
+
+  $(".wt-button").on('click', function(event) {
+    $("#" + event.target.id).css("border", "5px solid orange");
+    $("#" + event.target.id + "-c").addClass("wt1-c-1");
+  });
     
 });
